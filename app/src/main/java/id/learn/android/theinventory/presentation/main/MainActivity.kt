eaf601ce8864fd086bfcc94808eb075831df5768
deded <dededarirahmadi@gmail.com>
2021-06-17 21:06:57 +0700

    Initial commit

---
+++ app/src/main/java/id/learn/android/theinventory/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.learn.android.theinventory.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.learn.android.theinventory.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
