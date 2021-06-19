dc36af24efd606bcf8e2172d70bdcc6f162934de
deded <dededarirahmadi@gmail.com>
2021-06-19 23:54:44 +0700

    add deps

--- app/src/main/java/id/learn/android/theinventory/domain/model/Barang.kt
+++ app/src/main/java/id/learn/android/theinventory/domain/model/Barang.kt
@@ -1,8 +1,12 @@
 package id.learn.android.theinventory.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Barang(
         val idBarang: Int,
         val jmlStok: Int,
         val kondisi: String,
         val type: String
-)
+): Parcelable
